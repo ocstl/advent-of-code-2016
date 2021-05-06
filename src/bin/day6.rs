3855be721a7f54e6b939393a975d952e4f7193ac
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2021-05-06 11:53:31 -0400

    Day 6; remove duplication.

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -4,16 +4,6 @@ const INPUT_PATH: &str = "inputs/day6.txt";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input: String = std::fs::read_to_string(INPUT_PATH)?;
-
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
-
-    Ok(())
-}
-
-/// Given the recording in your puzzle input, what is the error-corrected
-/// version of the message being sent?
-fn part1(input: &str) -> String {
     let n = input.lines().next().unwrap().len();
     let mut counters = vec![Counter::<char>::new(); n];
 
@@ -23,6 +13,15 @@ fn part1(input: &str) -> String {
         }
     }
 
+    println!("Part 1: {}", part1(&counters));
+    println!("Part 2: {}", part2(&counters));
+
+    Ok(())
+}
+
+/// Given the recording in your puzzle input, what is the error-corrected
+/// version of the message being sent?
+fn part1(counters: &[Counter<char>]) -> String {
     counters
         .iter()
         .map(|counter| counter.most_common().unwrap())
@@ -31,16 +30,7 @@ fn part1(input: &str) -> String {
 
 /// Given the recording in your puzzle input and this new decoding methodology,
 /// what is the original message that Santa is trying to send?
-fn part2(input: &str) -> String {
-    let n = input.lines().next().unwrap().len();
-    let mut counters = vec![Counter::<char>::new(); n];
-
-    for line in input.lines() {
-        for (idx, c) in line.char_indices() {
-            counters[idx].add(c);
-        }
-    }
-
+fn part2(counters: &[Counter<char>]) -> String {
     counters
         .iter()
         .map(|counter| counter.least_common().unwrap())
